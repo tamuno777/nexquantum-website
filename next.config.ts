5cb3f5eea1ea565217c1fa33a2091c4e23a66a56
tamuno777 <davisbeulah07@gmail.com>
2024-12-02 16:01:06 +0100

    fixes

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["assets.zyrosite.com"], // Add your allowed domains here
-  },};
+  },
+  output: 'export',
+
+  experimental: {
+    // appDir: false,
+  },
+};
 
 export default nextConfig;
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["assets.zyrosite.com"], // Add your allowed domains here
-//   },
-// };
-
-// module.exports = nextConfig;
